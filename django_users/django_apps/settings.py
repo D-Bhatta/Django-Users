706dac4671165f08ed23902c9f799f4279fe0e06
D-Bhatta <dbhatta1232@gmail.com>
2020-11-17 06:22:19 -0500

    ref(setup) Integrate PostgreSQL 12 with project

--- django_users/django_apps/settings.py
+++ django_users/django_apps/settings.py
@@ -123,8 +123,12 @@ WSGI_APPLICATION = "django_apps.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.environ["DBNAME"],
+        "USER": os.environ["DBUSER"],
+        "PASSWORD": os.environ["DBPASSWORD"],
+        "HOST": os.environ["DBHOST"],
+        "PORT": os.environ["DBPORT"],
     }
 }
 
