43349feff47c21973799346b1079e5bce81f9b1a
D-Bhatta <dbhatta1232@gmail.com>
2020-11-21 15:53:38 -0500

    ref(views): Add user registration view
    Add a view to parse and save `NewUserCreationForm`
     form and display the form in a webpage

--- django_users/django_users/views.py
+++ django_users/django_users/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import redirect, render
+from django.urls import reverse
+
+from django_users.forms import NewUserCreationForm
 
 # Create your views here.
 
 
 def dashboard(request):
     return render(request, "dashboard.html", {})
+
+
+def register(request):
+    if request.method == "GET":
+        return render(request, "register.html", {"form": NewUserCreationForm})
+    elif request.method == "POST":
+        form = NewUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(reverse("dashboard"))
